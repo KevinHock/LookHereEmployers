4e76ef132e337cb2b2eb996b8d09d09039039ea4
Kevin Hock <kevin.hock@stonybrook.edu>
2014-12-22 18:17:00 -0500

    removed mcc's until 4 from 3 are done

--- matasano crypto challenges/Set One/6/6.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-double hamstrings(char *, char *);
-int hamchars(char, char);
-void turn_char_to_nib(int *, int);
-void removeChar(char *, char);
